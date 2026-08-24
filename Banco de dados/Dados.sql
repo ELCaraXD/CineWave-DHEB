ae6cdae7567dc96e9eb425dbdf05ade26b502b98
ELCaraXD <202413600011@ifba.edu.br>
2026-08-24 08:39:39 -0300

    Add movie records to filmes table
    Inserted multiple movie records into the filmes table with details including name, category, cast, release date, director, rating, and price.

--- Banco de dados/Dados.sql
+++ Banco de dados/Dados.sql
@@ -1,1 +1,47 @@
- 
+   INSERT INTO filmes (nome,categoria,elenco,lancamento,direcao,avaliacao,valor) 
+  VALUES (
+    '2001: Uma Odisseia no Espaço',
+    'ficcao cientifica',
+    'Dullea, Gary, Rositer, Silvester',
+    '1968-04-29',
+    'Stanley Kubrick',8.3,33.0);
+
+  INSERT INTO filmes (nome,categoria,elenco,lancamento,direcao,avaliacao,valor) 
+  VALUES (
+    'Ace Ventura: Um Detetive Diferente',
+    'Comedia',
+    'Jim Carrey, Sean Young, Udo Kier, Courteney Cox',
+    '1994-02-04',
+    'Tom Shadyac',6.9,25.0);
+
+  INSERT INTO filmes (nome,categoria,elenco,lancamento,direcao,avaliacao,valor) 
+  VALUES (
+    'Annabelle',
+    'Terror',
+    'Alfre Woodard, Annabelle Wallis, Tony Amendola, Ward Horton',
+    '2014-09-09',
+    'Jonh R. Leonneti',5.5,18.0);
+
+  INSERT INTO filmes (nome,categoria,elenco,lancamento,direcao,avaliacao,valor) 
+  VALUES (
+    'Como perder um homem em 10 dias',
+    'Comedia',
+    'Matthew McConaughey, Kate Hudson, Adam Goldberg, Kathryn Hahn',
+    '2003-04-17',
+    'Donald Petrie',6.5,27.0);
+
+  INSERT INTO filmes (nome,categoria,elenco,lancamento,direcao,avaliacao,valor) 
+  VALUES (
+    'Devoradores de estrelas',
+    'ficcao cientifica',
+    'Ryan Gosling, Sandra Huller, Milly Alcock, Lionel Boyce',
+    '2026-03-19',
+    'Phil Lord e Christopher Miller',8.3,37.0);
+
+  INSERT INTO filmes (nome,categoria,elenco,lancamento,direcao,avaliacao,valor) 
+  VALUES (
+    'Diario de uma paixao',
+    'Romance',
+    'Ryan Gosling, Rachel McAdams, James Garner, Gena Rowlands',
+    '2004-08-13',
+    'Nick Cassavetes',7.3,27.0);
